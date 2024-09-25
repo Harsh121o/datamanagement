681fb64ff34cdf0c313a98528b3cdc17ae3d77af
Harsh121o <singhalharsh187@gmail.com>
2024-09-25 16:15:59 -0500

    fix ath

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.use(session({
